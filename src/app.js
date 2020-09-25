f24debc96dbf2330495505954ed28bb0057e4311
junwoo45 <sinyoorang@gmail.com>
2020-09-25 16:54:04 +0900

    chore: style loader 테스트

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
+import "./style.css";
+
 console.log("this is app js");
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         test: /\.js$/, // .js 확장자로 끝나는 모든 파일
         use: [path.resolve("./myloader.js")], // 방금 만든 로더를 적용한다
       },
+      {
+        test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+        use: ["style-loader", "css-loader"], // 뒤에서부터 앞 순서로 로더가 동작한다
+      },
     ],
   },
 };
